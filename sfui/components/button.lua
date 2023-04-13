dd95565ad768b3c72f6a527a428f9d086e6b70a9
dmitrykorolev2009 <dmitrykorolev2009@gmail.com>
2023-04-13 13:49:24 +0300

    .

--- sfui/components/button.lua
+++ sfui/components/button.lua
@@ -7,13 +7,14 @@ function Button:initialize(parent, pos, size, text, callback)
 end
 
 function Button:render(cursor, action)
-    render.setColor(self.hover and self.palette.contrast or self.palette.component)
-    render.drawRoundedBox(self.mins.x, self.mins.y, self.size.x, self.size.y * self.aspectRatio)
 
-    render.setColor(self.action.held and self.palette.highlight or self.palette.contrast)
-    render.drawRoundedBox(self.mins.x, self.mins.y, self.size.x, self.size.y * self.aspectRatio)
+    render.setColor(self.action.held and self.palette.contrast or self.palette.hover)
+    render.drawRoundedBox(5, self.mins.x, self.mins.y, self.size.x, self.size.y * self.aspectRatio)
 
-    render.setColor(self.action.held and self.palette.highlight or self.palette.foreground)
+    render.setColor(5, Color(230, 230, 230))
+    render.drawRoundedBox(5, self.mins.x + 1, self.mins.y + 1, self.size.x - 2, (self.size.y - 2) * self.aspectRatio)
+
+    render.setColor(Color(0, 0, 0))
     render.drawSimpleText(self.center.x, self.center.y, self.text, TEXT_ALIGN.CENTER, TEXT_ALIGN.CENTER)
 
     if self.action.click and self.callback then
