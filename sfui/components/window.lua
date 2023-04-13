829468c62a6b8720e2ca4f50ac18009bdae5e66c
dmitrykorolev2009 <dmitrykorolev2009@gmail.com>
2023-04-13 15:57:00 +0300

    .

--- sfui/components/window.lua
+++ sfui/components/window.lua
@@ -130,8 +130,8 @@ function Window:render(cursor, action)
 
                 render.setColor(Color(255,255,255))
 
-                render.drawRectOutline(self.mins.x + self.size.x - self.barheight*2 + 4, self.mins.y + 4, self.barheight - 7, 3)
-                render.drawRectOutline(self.mins.x + self.size.x - self.barheight*2 + 4, self.mins.y + 4, self.barheight - 7, (self.barheight - 8) * self.aspectRatio, 1)
+                render.drawRectOutline(self.mins.x + self.size.x - self.barheight*2 + 4, self.mins.y + 5, self.barheight - 8, 3)
+                render.drawRectOutline(self.mins.x + self.size.x - self.barheight*2 + 4, self.mins.y + 5, self.barheight - 8, (self.barheight - 8) * self.aspectRatio, 1)
             end
             //
         end
