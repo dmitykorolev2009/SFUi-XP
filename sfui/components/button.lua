2d9a2d099ed30a8d0212ee045e9a7a903875eca6
dmitrykorolev2009 <dmitrykorolev2009@gmail.com>
2023-04-13 13:57:14 +0300

    .

--- sfui/components/button.lua
+++ sfui/components/button.lua
@@ -12,7 +12,7 @@ function Button:render(cursor, action)
     render.drawRoundedBox(5, self.mins.x, self.mins.y, self.size.x, self.size.y * self.aspectRatio)
 
     render.setColor(Color(230, 230, 230))
-    render.drawRoundedBox(5, self.mins.x + 3, self.mins.y + 3, self.size.x - 6, (self.size.y - 6) * self.aspectRatio)
+    render.drawRoundedBox(5, self.mins.x + 3, self.mins.y + 3 * self.aspectRatio, self.size.x - 6, (self.size.y - 6) * self.aspectRatio)
 
     render.setColor(Color(0, 0, 0))
     render.drawSimpleText(self.center.x, self.center.y, self.text, TEXT_ALIGN.CENTER, TEXT_ALIGN.CENTER)
